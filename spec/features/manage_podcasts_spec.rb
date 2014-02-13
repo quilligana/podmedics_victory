4661c1b629798d5ecd52c7ee954515dafc72cb4a
Edward Wallitt <edwallitt@me.com>
2014-02-13 18:33:14 +0000

    corrected spec_helper and refactor manage video spec

--- spec/features/manage_podcasts_spec.rb
+++ spec/features/manage_podcasts_spec.rb
@@ -6,30 +6,38 @@ feature 'Managing videos' do
   scenario 'Adding a new video' do
     specialty = create(:specialty)
     sign_in(admin_user)
-    click_link 'Videos'
-    click_link 'New video'
-    fill_in 'Title', with: 'The ECG'
-    fill_in 'Description', with: 'This is a podcast about ECGs'
-    select specialty.name, from: 'Specialty'
-    fill_in 'Vimeo Identifier', with: '324567'
-    fill_in 'Duration', with: '10'
-    click_button 'Submit'
-
-    expect(current_path).to eq admin_videos_path
-    expect(page).to have_content 'The ECG'
+    add_podcast_with_name_and_specialty('The ECG', specialty)
+    admin_sees_podcast_name 'The ECG'
   end
 
   scenario  'Editing a video' do
     video = create(:video)
     sign_in(admin_user)
+    edit_podcast_to_name 'Heart Failure'
+    admin_sees_podcast_name 'Heart Failure'
+  end
+
+  def add_podcast_with_name_and_specialty(name, specialty)
+    click_link 'Videos'
+    click_link 'New video'
+    fill_in 'Title', with: name
+    fill_in 'Description', with: 'This is a podcast about ECGs'
+    select specialty.name, from: 'Specialty'
+    fill_in 'Vimeo Identifier', with: '324567'
+    fill_in 'Duration', with: '10'
+    click_button 'Submit'
+  end
+
+  def edit_podcast_to_name(name)
     click_link 'Videos'
     within 'li.video' do
       click_link 'Edit'
     end
-    fill_in 'Title', with: 'Heart Failure'
+    fill_in 'Title', with: name
     click_button 'Submit'
+  end
 
-    expect(page).to have_content 'Heart Failure'
-
+  def admin_sees_podcast_name(name)
+    expect(page).to have_content name
   end
 end
