f834231de5a50f116989653f0c7e41f83d206753
Edward Wallitt <edwallitt@me.com>
2014-03-05 15:59:24 +0000

    add user internal navigation links

---
+++ spec/features/user_navigation_spec.rb
@@ -0,0 +1,35 @@
+require "spec_helper"
+
+feature 'User Navigation' do
+
+  before :each do
+    sign_in(create(:user))
+  end
+
+  scenario 'sees logout link' do
+    user_sees_link 'Logout'
+  end
+
+  scenario 'sees profile link' do
+    user_sees_link 'Your Profile'
+  end
+
+  scenario 'sees courses link' do
+    user_sees_link 'Courses'
+  end
+
+  scenario 'sees lectures link' do
+    user_sees_link 'Lectures'
+  end
+
+  scenario 'sees support link' do
+    user_sees_link 'Support'
+  end
+
+  # Helpers
+  def user_sees_link(link_name)
+    within '.nav' do
+      expect(page).to have_link link_name
+    end
+  end
+end
