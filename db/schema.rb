3b2fd6d3c3698944d5fe4179567d6913690a6704
Aidan Quilligan <a.quilligan@gmail.com>
2014-03-26 19:53:26 +0000

    User model updated

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140323140050) do
+ActiveRecord::Schema.define(version: 20140326194841) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -91,6 +91,7 @@ ActiveRecord::Schema.define(version: 20140323140050) do
     t.string   "name"
     t.string   "oauth_token"
     t.datetime "oauth_expires_at"
+    t.integer  "points"
   end
 
   create_table "videos", force: true do |t|
