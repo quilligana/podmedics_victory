928996a528bd13adf64a7781b7110a636359ed46
Edward Wallitt <edwallitt@me.com>
2014-04-01 14:00:41 +0100

    add courses page

--- app/models/permission.rb
+++ app/models/permission.rb
@@ -3,6 +3,7 @@ class Permission < Struct.new(:user)
   def allow?(controller, action)
     return true if controller == 'static_pages'
     return true if controller == 'sessions'
+    return true if controller == 'courses'
     return true if controller == 'users' && action == 'new'
     return true if controller == 'users' && action == 'create'
     if user
