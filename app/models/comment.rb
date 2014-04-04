b02d6f30ed9ab4cdd5e828c8f6ca5a6586a25d8b
Daniel Lockhart <daniel@lockyy.com>
2014-04-04 22:18:21 +0100

    Makes comment form reset to commenting on the video after submitting a reply to someone.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -7,4 +7,15 @@ class Comment < ActiveRecord::Base
   validates :user, presence: true
   validates :content, presence: true
   validates :commentable, presence: true
+
+  # Navigates up through a comment tree to get the original commentable e.g. a video
+  def get_root_commentable
+    comment_parent = self
+    
+    while comment_parent.commentable_type == "Comment" do
+      comment_parent = comment_parent.commentable
+    end
+
+    return comment_parent.commentable
+  end
 end
