ab01dcba3bebaf9bd86c0545aa4bbb3bd0a571e1
Daniel Lockhart <daniel@lockyy.com>
2014-04-05 17:40:04 +0100

    Fixes typo in comment.comment_count

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -30,7 +30,7 @@ class Comment < ActiveRecord::Base
     count = comments.count
 
     comments.each() do |comment|
-      count += comment.comments_count(only_visible)
+      count += comment.comment_count(only_visible)
     end
 
     return count
