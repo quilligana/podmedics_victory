e625e63150b49c33330bbc9a38bd95cf8505d9ae
Daniel Lockhart <daniel@lockyy.com>
2014-04-07 17:21:07 +0100

    5-7x speed up in comment tests. Added in test for when a comment is posted after a reply.

--- spec/features/comments_spec.rb
+++ spec/features/comments_spec.rb
@@ -12,9 +12,10 @@ describe "video comments" do
   subject { page }
 
   describe "before posting a comment" do
-    it { should have_content("Nothing posted yet. Be the first to comment.") }
-    it { should have_content("Comments (0)") }
-    it { should_not have_content(@user.name) }
+    it "should not have any comments" do
+      expect(page).to have_content("Nothing posted yet. Be the first to comment.")
+      expect(page).to have_content("Comments (0)")
+    end
   end
 
   describe "posting a comment" do
@@ -25,13 +26,15 @@ describe "video comments" do
       end
 
       Capybara.using_wait_time 10 do
-        it { should have_css( "div.comment div.inner_page_padding div.comment_info p", 
-                              text: "This is a comment.") }
-        it { should have_selector("div.comment") }
-        it { should have_content("This is a comment.") }
-        it { should_not have_content("Nothing posted yet. Be the first to comment.") }
-        it { should have_content(@user.name) }
-        it { should have_content("Comments (1)") }
+        it "should have a new comment" do
+          expect(page).to     have_content(@user.name)
+          expect(page).to     have_content("Comments (1)")
+          expect(page).to     have_selector("div.comment")
+          expect(page).to     have_content("This is a comment.")
+          expect(page).to     have_css( "div.comment div.inner_page_padding div.comment_info p", 
+                                        text: "This is a comment.")
+          expect(page).to_not have_content("Nothing posted yet. Be the first to comment.")
+        end
       end
     end
 
@@ -42,14 +45,16 @@ describe "video comments" do
       end
 
       Capybara.using_wait_time 10 do
-        it { should_not have_css( "div.comment div.inner_page_padding div.comment_info p", 
-                                  text: "This is a comment.") }
-        it { should_not have_content("This is a comment.") }
-        it { should have_content("Nothing posted yet. Be the first to comment.") }
-        it { should_not have_content(@user.name) }
-        it { should have_content("ERROR") }
-        it { should have_content("There has been a problem") }
-        it { should have_content("Comments (0)") }
+        it "should not have any comments" do
+          expect(page).to     have_content("ERROR")
+          expect(page).to     have_content("Comments (0)")
+          expect(page).to     have_content("There has been a problem")
+          expect(page).to     have_content("Nothing posted yet. Be the first to comment.")
+          expect(page).to_not have_content(@user.name)
+          expect(page).to_not have_content("This is a comment.")
+          expect(page).to_not have_css( "div.comment div.inner_page_padding div.comment_info p", 
+                                        text: "This is a comment.")
+        end
       end
     end
 
@@ -67,12 +72,45 @@ describe "video comments" do
       end
 
       Capybara.using_wait_time 10 do
-        it { should have_css( "div.comment_reply div.comment div.inner_page_padding div.comment_info p", 
-                              text: "This is a reply.") }
-        it { should have_selector("div.comment_reply") }
-        it { should have_content("This is a reply.") }
-        it { should have_content(@user.name) }
-        it { should have_content("Comments (2)") }
+        it "should have a reply comment" do
+          expect(page).to     have_content(@user.name)
+          expect(page).to     have_content("Comments (2)")
+          expect(page).to     have_content("This is a reply.")
+          expect(page).to     have_selector("div.comment_reply")
+          expect(page).to     have_css( "div.comment_reply div.comment div.inner_page_padding div.comment_info p", 
+                                        text: "This is a reply.")
+        end
+      end
+    end
+
+    describe "commenting after replying", :js => true do
+      before do
+        fill_in "comment_content", with: "This is a comment."
+        click_button "Create Comment"
+
+        sleep 1.seconds
+
+        fill_in "comment_commentable_id", with: Comment.first.id
+        fill_in "comment_commentable_type", with: "Comment"
+        fill_in "comment_content", with: "This is a reply."
+        click_button "Create Comment"
+
+        sleep 1.seconds
+
+        fill_in "comment_content", with: "This is a comment after a reply."
+        click_button "Create Comment"
+      end
+
+      Capybara.using_wait_time 10 do
+        it "should have a new non-reply comment" do
+          expect(page).to      have_content(@user.name)
+          expect(page).to      have_selector("div.comment")
+          expect(page).to      have_content("Comments (3)")
+          expect(page).to      have_content("This is a comment after a reply.")
+          expect(page).to      have_css( "div.comment div.inner_page_padding div.comment_info p", 
+                                         text: "This is a comment after a reply.")
+          expect(page).to_not have_content("Nothing posted yet. Be the first to comment.")
+        end
       end
     end
   end
