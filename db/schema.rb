33ad9e52bb6da62d06408eb4f419a0c535590e4e
Daniel Lockhart <daniel@lockyy.com>
2014-04-08 16:54:11 +0100

    Add speciality question model.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140407140504) do
+ActiveRecord::Schema.define(version: 20140408154602) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,7 +40,8 @@ ActiveRecord::Schema.define(version: 20140407140504) do
     t.datetime "updated_at"
     t.integer  "user_id"
     t.boolean  "hidden",           default: false
-    t.integer  "video_id"
+    t.integer  "root_id"
+    t.string   "root_type"
   end
 
   create_table "courses", force: true do |t|
@@ -89,6 +90,13 @@ ActiveRecord::Schema.define(version: 20140407140504) do
   end
 
   add_index "questions", ["video_id"], name: "index_questions_on_video_id", using: :btree
+
+  create_table "speciality_questions", force: true do |t|
+    t.string   "content"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "specialties", force: true do |t|
     t.string   "name"
