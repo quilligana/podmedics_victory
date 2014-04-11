0e19064a28e413800ff0ca396223de98a30c9a4b
Daniel Lockhart <daniel@lockyy.com>
2014-04-11 19:05:28 +0100

    Adds space after @name when you hit reply.

--- app/assets/javascripts/site.js
+++ app/assets/javascripts/site.js
@@ -27,7 +27,7 @@ ready = function() {
   $(document).on("click", ".comment_reply_link a", function(){
     var comment_info = $(this).parents(".comment_info")
     var comment_username = $(comment_info).find("h3.comment_username")[0].innerHTML;
-    $(".comments_textarea").val('@' + comment_username).focus().setCursorToTextEnd();
+    $(".comments_textarea").val('@' + comment_username + ' ').focus().setCursorToTextEnd();
     $(window).scrollTop($("#comment_reply_textarea").offset().top);
     $("#comment_commentable_type").val('Comment');
     $("#comment_commentable_id").val(this.id);
