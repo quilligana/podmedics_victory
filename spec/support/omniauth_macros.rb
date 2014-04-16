aeb6e19fafc9910f47ebdefd77cb4b5f5b863b8a
Daniel Lockhart <daniel@lockyy.com>
2014-04-16 15:20:42 +0100

    Fix mock omniauth values for facebook/twitter to make tests work correctly.

--- spec/support/omniauth_macros.rb
+++ spec/support/omniauth_macros.rb
@@ -3,16 +3,16 @@ module OmniauthMacros
 	def mock_twitter_auth_hash()
 		OmniAuth.config.mock_auth[:twitter] = OmniAuth::AuthHash.new(
 		{
-		  :provider => 'twitter',
-		  :uid => '123545',
-		  :info => 
+		  provider: 'twitter',
+		  uid: '123545',
+		  info: 
 		  {
-		    :name => 'mockuser'
+		    name: 'mockuser'
 		  },
-		  :credentials => 
+		  credentials:
 		  {
-		    :token => 'mock_token',
-		    :secret => 'mock_secret'
+		    token: 'mock_token',
+		    secret: 'mock_secret'
 		  }
 		})
 	end
@@ -20,17 +20,18 @@ module OmniauthMacros
 	def mock_facebook_auth_hash()
 		OmniAuth.config.mock_auth[:facebook] = OmniAuth::AuthHash.new(
 		{
-		  :provider => 'twitter',
-		  :uid => '123545',
-		  :info => 
+		  provider: 'facebook',
+		  uid: '123545',
+		  info: 
 		  {
-		    :name => 'mockuser',
-		    :email => 'mock@email.com'
+		    name: 'mockuser',
+		    email: 'mail@example.com'
 		  },
-		  :credentials => 
+		  credentials:
 		  {
-		    :token => 'mock_token',
-		    :secret => 'mock_secret'
+		    token: 'mock_token',
+		    secret: 'mock_secret',
+		    expires_at: 7.days.since(Time.now).to_i
 		  }
 		})
 	end
