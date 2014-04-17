8536a364d436cf72ecef1c6166058c357e76a771
Edward Wallitt <edwallitt@me.com>
2014-04-17 15:43:29 +0100

    add factory for notes and added further test to notes model

--- spec/models/note_spec.rb
+++ spec/models/note_spec.rb
@@ -7,4 +7,13 @@ describe Note do
   it { should belong_to :video }
   it { should validate_presence_of :content }
 
+  describe Note, '#get_title' do
+    it "sets the title to the video title if not set" do
+      video = create(:video)
+      note = build(:note, video: video, title: '')
+      expect(note.get_title).to eq video.title
+    end
+    
+  end
+
 end
