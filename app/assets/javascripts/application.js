54aeb9bb4756ca4e77a9424e23e6e46fa9dcbb52
Edward Wallitt <edwallitt@me.com>
2014-05-02 13:05:49 +0100

    refactored public facing js to only include required js modules and hence reduce errors

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,10 +1,38 @@
 //= require jquery
 //= require jquery_ujs
-//= require tabulous
-//= require icheck
 //= require fresco
-//= require jquery.fitvids
-//= require froogaloop
-//= require site
-//= require autosave
-//= require vimeo
+
+$(document).ready(function(){
+
+  // Basic menu functions
+  $("#menu_action").click(function(){
+    $("body").addClass('cbp-spmenu-push-toleft');
+    $(".cbp-spmenu-right").addClass('active_menu');
+    $(".navigation_outer_overlay").fadeIn(200);
+  });
+
+  $(".navigation_outer_overlay").click(function(){
+    $(this).fadeOut(200);
+    $("body").removeClass('cbp-spmenu-push-toleft').addClass('default_body');
+    $(".cbp-spmenu-right").removeClass('active_menu');
+  });
+
+  // Library dropdowns
+  $('a.library_button').on('click', function(e){
+    $(this).toggleClass('active');
+    $(this).next('.library_tab').slideToggle(400);
+  });
+
+  // Forgot password link
+  $("#forgot_password_link").click(function(){
+    $("#members_login_form").fadeOut(200);
+    $("#members_reset_password_form").delay(200).fadeIn(200);
+  });
+
+  // Forgot password back link
+  $("#forgot_password_back_link").click(function(){
+    $("#members_reset_password_form").fadeOut(200);
+    $("#members_login_form").delay(200).fadeIn(200);
+  });
+
+});
