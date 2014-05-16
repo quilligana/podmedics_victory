3cbdd6d6dee571b9a5b69ed3164b0a008c238389
Edward Wallitt <edwallitt@me.com>
2014-05-16 14:20:00 +0100

    add upgrade message partial to dashboard

---
+++ spec/features/upgrade_plan_spec.rb
@@ -0,0 +1,15 @@
+require 'spec_helper'
+
+feature 'Upgrade Plan' do
+
+  before do
+    @user = create(:user)
+  end
+
+  scenario 'Logging as a free plan member I should see an upgrade link' do
+    sign_in(@user)
+    expect(current_path).to eq dashboard_path
+    expect(page).to have_link 'Upgrade your plan'
+  end
+
+end
