3b8f2e339123814bcf8b8681ad7fdd6d5315ddb6
Jamie Peak <jamie@podmedics.com>
2014-05-16 13:36:52 +0100

    add styling to stripe button

--- db/schema.rb
+++ db/schema.rb
@@ -170,13 +170,6 @@ ActiveRecord::Schema.define(version: 20140516104934) do
   add_index "sales", ["product_id"], name: "index_sales_on_product_id", using: :btree
   add_index "sales", ["user_id"], name: "index_sales_on_user_id", using: :btree
 
-  create_table "speciality_questions", force: true do |t|
-    t.string   "content"
-    t.integer  "user_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
   create_table "specialties", force: true do |t|
     t.string   "name"
     t.integer  "category_id"
