20a4bdca9a1a5fc3cd174a35eb6500d7563d7329
Edward Wallitt <edwallitt@me.com>
2014-05-17 12:28:17 +0100

    remove extra delay on episode notifications

--- app/controllers/admin/videos_controller.rb
+++ app/controllers/admin/videos_controller.rb
@@ -27,7 +27,7 @@ class Admin::VideosController < InheritedResources::Base
   end
 
   def send_notifications
-    @video.delay.send_video_notification
+    @video.send_video_notification
     redirect_to :back
   end
 
