77b4e4e3d093cc8c883a83fd33d0b6d97a9650a2
Daniel Lockhart <daniel@lockyy.com>
2014-05-18 14:35:07 +0100

    Fix tests. Add more mailer tests

--- spec/mailers/user_mailer_spec.rb
+++ spec/mailers/user_mailer_spec.rb
@@ -11,6 +11,20 @@ describe UserMailer do
       mail.from.should eq(["admin@podmedics.com"])
       mail.body.encoded.should match(edit_password_reset_path(user.password_reset_token))
     end
+
+    describe "when the user has unsubscribed" do
+      before do
+        user.unsubscribe
+        @mail = UserMailer.password_reset(user)
+      end
+
+      it "will still send the email" do
+        @mail.subject.should eq("Podmedics Password Reset")
+        @mail.to.should eq([user.email])
+        @mail.from.should eq(["admin@podmedics.com"])
+        @mail.body.encoded.should match(edit_password_reset_path(user.password_reset_token))
+      end
+    end
   end
 
   describe "badge_award" do
@@ -24,6 +38,17 @@ describe UserMailer do
       mail.to.should eq([user.email])
       mail.from.should eq(["admin@podmedics.com"])
     end
+
+    describe "when the user has unsubscribed" do
+      before do
+        user.unsubscribe
+        @mail = UserMailer.badge_award(user, badge)
+      end
+
+      it "will not send the email" do
+        @mail.subject.should eq nil
+      end
+    end
   end
 
   describe "professor_loss" do
@@ -35,6 +60,89 @@ describe UserMailer do
       mail.subject.should eq("Notification of Your Professor Status")
       mail.to.should eq([user.email])
       mail.from.should eq(["admin@podmedics.com"])
+    end
+
+    describe "when the user has unsubscribed" do
+      before do
+        user.unsubscribe
+        @mail = UserMailer.professor_loss(user, specialty)
+      end
+
+      it "will not send the email" do
+        @mail.subject.should eq nil
+      end
+    end
+  end
+
+  describe "new_episode" do
+    let(:user) { create(:user) }
+    let(:video) { create(:video) }
+    let(:mail) { UserMailer.new_episode(user, video) }
+
+    it "notifies the user of their loss of professor badge" do
+      mail.subject.should eq("There is a new video: #{video.title}")
+      mail.to.should eq([user.email])
+      mail.from.should eq(["admin@podmedics.com"])
+    end
+
+    describe "when the user has unsubscribed" do
+      before do
+        user.unsubscribe
+        @mail = UserMailer.new_episode(user, video)
+      end
+
+      it "will not send the email" do
+        @mail.subject.should eq nil
+      end
+    end
+  end
+
+  describe "new_reply" do
+    let(:user) { create(:user) }
+    let(:video) { create(:video) }
+    let(:comment) { create(:comment, commentable: video, root: video) }
+    let(:reply) { create(:comment, commentable: comment, root: video) }
+    let(:mail) { UserMailer.new_reply(user, comment, reply) }
+
+    it "notifies the user of their loss of professor badge" do
+      mail.subject.should eq("You have a new reply")
+      mail.to.should eq([user.email])
+      mail.from.should eq(["admin@podmedics.com"])
+    end
+
+    describe "when the user has unsubscribed" do
+      before do
+        user.unsubscribe
+        @mail = UserMailer.new_reply(user, comment, reply)
+      end
+
+      it "will not send the email" do
+        @mail.subject.should eq nil
+      end
+    end
+  end
+
+  describe "answer_accepted" do
+    let(:user) { create(:user) }
+    let(:specialty_question) { create(:specialty_question) }
+    let(:answer) { create(:comment, commentable: specialty_question, root: specialty_question) }
+    let(:mail) { UserMailer.answer_accepted(user, answer) }
+
+    it "notifies the user of their loss of professor badge" do
+      mail.subject.should eq('One of your answers has been accepted')
+      mail.to.should eq([user.email])
+      mail.from.should eq(["admin@podmedics.com"])
+    end
+
+    describe "when the user has unsubscribed" do
+      before do
+        user.unsubscribe
+        @mail = UserMailer.answer_accepted(user, answer)
+      end
+
+      it "will not send the email" do
+        @mail.subject.should eq nil
+      end
     end
   end
 end
