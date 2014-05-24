71d4eba1728816b020cd6728e3435328ae54d3f0
Edward Wallitt <edwallitt@me.com>
2014-05-24 12:54:49 +0100

    enable vimeo ssl for admin

--- db/schema.rb
+++ db/schema.rb
@@ -15,6 +15,7 @@ ActiveRecord::Schema.define(version: 20140523125530) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+  enable_extension "pg_stat_statements"
 
   create_table "authors", force: true do |t|
     t.string   "name"
