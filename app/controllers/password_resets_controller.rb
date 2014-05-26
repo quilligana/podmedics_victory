7cc2401eba502f09cdbc1a3693539dc0ce911862
Edward Wallitt <edwallitt@me.com>
2014-05-26 14:19:06 +0100

    improved password reset flow if email is not found

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -2,8 +2,12 @@ class PasswordResetsController < ApplicationController
 
   def create
     user = User.find_by_email(params[:email])
-    user.send_password_reset if user
-    redirect_to root_url, notice: 'Email sent with password reset instructions.'
+    if user
+      user.send_password_reset
+      redirect_to root_url, notice: 'Email sent with password reset instructions.'
+    else
+      redirect_to login_path, alert: 'Sorry we could not find that email address. Did you register with a different one?'
+    end
   end
 
   def edit
@@ -12,7 +16,7 @@ class PasswordResetsController < ApplicationController
 
   def update
     @user = User.find_by_password_reset_token!(params[:id])
-    if @user.password_reset_sent_at < 2.hours.ago
+    if @user.password_reset_sent_at < 6.hours.ago
       redirect_to login_path, alert: 'Password reset has expired.'
     elsif @user.update_attributes(user_params)
       redirect_to root_url, notice: 'Password has been reset!'
--- spec/features/password_resets_spec.rb
+++ spec/features/password_resets_spec.rb
@@ -13,4 +13,15 @@ feature 'Password Resets' do
     expect(page).to have_content 'Email sent'
     expect(last_email.to).to include(user.email)
   end
+
+  scenario 'Showing an error message when no email is found' do
+    visit login_path
+    within '#members_reset_password_form' do
+      fill_in 'Email', with: 'fake_email@example.com' 
+      click_button 'Reset Password'
+    end
+    expect(current_path).to eq login_path
+    expect(page).to have_content 'Sorry we could not find that email address. Did you register with a different one?'
+
+  end
 end
