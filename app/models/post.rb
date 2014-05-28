2b73de788db9198c3b6142f2ec7978a7466d5ce5
Edward Wallitt <edwallitt@me.com>
2014-05-28 16:13:21 +0100

    refactored posts system

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,5 +2,7 @@ class Post < ActiveRecord::Base
 
   belongs_to :author
   validates :title, :content, presence: true
+  delegate :twitter, to: :author, prefix: true
+  delegate :facebook, to: :author, prefix: true
 
 end
