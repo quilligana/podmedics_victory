ba207f6106dc25cf4cf4333fb45b31030753cd69
Daniel Lockhart <daniel@lockyy.com>
2014-06-03 12:10:03 +0100

    Remove links to specialty/category notes pages from each set of notes in /notes. Add links to specialty/category headers in /notes. Add link to a note's notable's page to each set of notes.

--- db/schema.rb
+++ db/schema.rb
@@ -15,7 +15,6 @@ ActiveRecord::Schema.define(version: 20140602152854) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-  enable_extension "pg_stat_statements"
 
   create_table "authors", force: true do |t|
     t.string   "name"
