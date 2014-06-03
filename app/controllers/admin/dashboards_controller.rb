3d31da829fe594345a81045bd487c60560422ccf
Edward Wallitt <edwallitt@me.com>
2014-06-03 10:11:06 +0100

    comment out gattica for now

--- app/controllers/admin/dashboards_controller.rb
+++ app/controllers/admin/dashboards_controller.rb
@@ -3,22 +3,22 @@ class Admin::DashboardsController < ApplicationController
   layout 'admin_application'
 
   def show
-    ga = Gattica.new({
-      email: ENV['GOOGLE_EMAIL'],
-      password: ENV['GOOGLE_PASS'] 
-    })
-    puts ga.token
-    ga.profile_id = 41887443
+    #ga = Gattica.new({
+      #email: ENV['GOOGLE_EMAIL'],
+      #password: ENV['GOOGLE_PASS'] 
+    #})
+    #puts ga.token
+    #ga.profile_id = 41887443
 
-    data = ga.get({
-      start_date: '2014-01-01',
-      end_date: '2014-03-04',
-      dimensions: ['month', 'year'],
-      metrics: ['visitors']
-    })
+    #data = ga.get({
+      #start_date: '2014-01-01',
+      #end_date: '2014-03-04',
+      #dimensions: ['month', 'year'],
+      #metrics: ['visitors']
+    #})
 
-    my_json = data.to_h['points'].to_json
-    puts my_json
+    #my_json = data.to_h['points'].to_json
+    #puts my_json
 
   end
 
