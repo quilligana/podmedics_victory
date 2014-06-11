126387d4d00ef2f815ecfd6c1b18464d4dd2395f
Daniel Lockhart <daniel@lockyy.com>
2014-06-11 13:14:57 +0100

    Move email model out into root concerns directory

---
+++ app/models/concerns/email.rb
@@ -0,0 +1,25 @@
+module Email
+  extend ActiveSupport::Concern
+
+  included do
+    before_create :generate_unsubscribe_token
+  end
+
+  module ClassMethods
+    def episode_notifications_allowed
+      where(receive_new_episode_notifications: true)
+    end
+
+    def newsletters_allowed
+      where(receive_newsletters: true)
+    end
+  end
+
+  def generate_unsubscribe_token
+    generate_token(:unsubscribe_token)
+  end
+
+  def unsubscribe(column)
+    self[:column] = false
+  end
+end
