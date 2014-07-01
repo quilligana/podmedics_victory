b80514ac5e02d7a0b2e54ed42722c1cc54ef3804
Edward Wallitt <edwallitt@me.com>
2014-07-01 15:19:15 +0100

    dahsboard; remove chart, remove recent and recommended

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -2,11 +2,11 @@ class DashboardsController < ApplicationController
   layout 'user_application'
     
   def show
-    @recent_videos = Video.recent.limit(10).includes(:specialty)
-    @flagged_videos = Video.flagged(current_user).first(10)
+    #@recent_videos = Video.recent.limit(10).includes(:specialty)
+    #@flagged_videos = Video.flagged(current_user).first(10)
     @badges = current_user.badges
-    @percentiles = User.percentile_stat
-    @user_percentile = current_user.percentile
+    #@percentiles = User.percentile_stat
+    #@user_percentile = current_user.percentile
   end
 
 end
