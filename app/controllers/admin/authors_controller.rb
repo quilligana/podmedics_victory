658d9876a10accd06d965fd922f345ef497c9ecb
Edward Wallitt <edwallitt@me.com>
2014-07-14 17:40:38 +0100

    remove inherited resources from admin author controller

--- app/controllers/admin/authors_controller.rb
+++ app/controllers/admin/authors_controller.rb
@@ -2,18 +2,43 @@ class Admin::AuthorsController < InheritedResources::Base
   layout 'admin_application'
   respond_to :html
 
-  def create
-    create!(notice: 'New Author added') { admin_authors_path }
+  def index
+    @authors = Author.all
   end
 
-  protected
+  def new
+    @author = Author.new
+  end
 
-    def collection
-      @authors ||= end_of_association_chain.includes(:videos)
+  def show
+    @author = Author.find(params[:id])
+  end
+
+  def create
+    @author = Author.new(author_params)
+    if @author.save
+      redirect_to admin_authors_path, notice: 'Author created'
+    else
+      render :new
     end
+  end
 
-    def permitted_params
-      params.permit(:author => [:name, :tagline, :twitter, :facebook, :avatar])
+  def update
+    @author = Author.find(params[:id])
+    if @author.update_attributes(author_params)
+      redirect_to admin_author_path(@author), notice: 'Author updated'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+  end
+
+  private
+
+    def author_params
+      params.require(:author).permit(:name, :tagline, :twitter, :facebook, :avatar)
     end
 
 end
