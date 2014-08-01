cd0bfbbf11341f29f78fddc6d22b6655eba39164
Edward Wallitt <edwallitt@me.com>
2014-08-01 16:07:27 +0100

    rework handling of file downloads

--- app/controllers/hosted_files_controller.rb
+++ app/controllers/hosted_files_controller.rb
@@ -4,38 +4,38 @@ class HostedFilesController < ApplicationController
 
   def video
     Video.increment_counter(:video_download_count, @video.id)
-    url = "http://podmedics-test-harness.s3.amazonaws.com/#{@video.file_name}.mp4"
-    download_url(url)
+    s3_name = "#{@video.file_name}.mp4"
+    redirect_to generate_download_url(s3_name)
   end
 
   def audio
     Video.increment_counter(:audio_download_count, @video.id)
-    url = "http://podmedics-test-harness.s3.amazonaws.com/audio/#{@video.file_name}.mp3"
-    download_url(url)
+    s3_name = "audio/#{@video.file_name}.mp3"
+    redirect_to generate_download_url(s3_name)
   end
 
   def slides
     Video.increment_counter(:slide_download_count, @video.id)
-    url = "http://podmedics-test-harness.s3.amazonaws.com/slides/#{@video.file_name}.pdf"
-    download_url(url)
+    s3_name = "slides/#{@video.file_name}.pdf"
+    redirect_to generate_download_url(s3_name)
   end
 
   private
+
+    def generate_download_url(file_path)
+      s3 = AWS::S3.new(
+        :access_key_id => ENV["AWS_ACCESS_ID"],
+        :secret_access_key => ENV["AWS_SECRET_ACCESS"]
+      )
+      bucket = s3.buckets['podmedics-test-harness']
+      object = bucket.objects[file_path]
+      url = object.url_for(:read, :expires => 10*60).to_s
+    end
 
     def check_subscription
       if current_user && current_user.is_trial_member?
         redirect_to @video, alert: 'Sorry! Downloads are only available to full members'
       end
-    end
-
-    def download_url(url)
-      resp = HTTParty.get(url)
-      filename  = url
-      send_data resp.body,
-        :filename => File.basename(filename),
-        :disposition => "attachment"
-      # start garbage collection manually to fix memory quote exceeded
-      GC.start
     end
 
     def find_video
