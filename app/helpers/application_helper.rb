b69049ef70521ffef6b3680b309dd1fe59a5d932
Edward Wallitt <edwallitt@me.com>
2014-10-09 11:19:06 +0100

    fix error in analytics code, add gravatar code but not using yet

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -47,4 +47,10 @@ module ApplicationHelper
     end
   end
 
+  def gravatar_url(email, size)
+    gravatar_id = Digest::MD5::hexdigest(email).downcase
+    default_url = ActionController::Base.helpers.asset_url('avatar-128.jpg')
+    url = "http://gravatar.com/avatar/#{gravatar_id}.png?s=#{size}&d=#{CGI::escape(default_url)}"
+  end
+
 end
