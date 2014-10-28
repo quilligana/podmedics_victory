1496a9644bb1ed50f782184abb61feb09f78b590
jamiepeak <me@jamiepeak.co.uk>
2014-10-28 11:10:17 +0000

    responsive update to app quiz page

--- db/schema.rb
+++ db/schema.rb
@@ -15,7 +15,6 @@ ActiveRecord::Schema.define(version: 20141016122623) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-  enable_extension "pg_stat_statements"
 
   create_table "authors", force: true do |t|
     t.string   "name"
@@ -211,13 +210,6 @@ ActiveRecord::Schema.define(version: 20141016122623) do
 
   add_index "sales", ["product_id"], name: "index_sales_on_product_id", using: :btree
   add_index "sales", ["user_id"], name: "index_sales_on_user_id", using: :btree
-
-  create_table "speciality_questions", force: true do |t|
-    t.string   "content"
-    t.integer  "user_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
 
   create_table "specialties", force: true do |t|
     t.string   "name"
