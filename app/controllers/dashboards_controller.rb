e50133db45f56e895f0cd950fc4ea134d9f488ba
Edward Wallitt <edwallitt@me.com>
2014-11-19 23:26:14 +0000

    remove redudant code from dashboard controller

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -5,9 +5,6 @@ class DashboardsController < ApplicationController
     @recent_videos = Video.recent.includes(:specialty).limit(10)
     @flagged_videos = Video.flagged(current_user).first(10)
     @badges = current_user.badges.includes(:specialty)
-    @recent_questions = SpecialtyQuestion.includes(:specialty, :user).recent(3) 
-    #@percentiles = User.percentile_stat
-    #@user_percentile = current_user.percentile
   end
 
 end
