c9205a76b6a378b96f3895cdbf7550bbde02088a
Edward Wallitt <edwallitt@me.com>
2014-11-24 12:47:13 +0000

    update handling of authentication token to invoke null_session rather than exception

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
+  protect_from_forgery with: :null_session
   include SessionsHelper
   before_action :authorise
   before_action :get_content
