6f66ec0842f8992f8ec337444f3640ab41e56695
Aidan Quilligan <a.quilligan@gmail.com>
2015-01-06 16:25:18 +0000

    Paperclip url generation.

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,3 +1,4 @@
 # required for amazon s3 eu location
 Paperclip::Attachment.default_options[:s3_host_name] = 's3-eu-west-1.amazonaws.com'
 Paperclip::Attachment.default_options[:s3_protocol] = :https
+Paperclip::Attachment.default_options[:url] = ":s3_domain_url"
