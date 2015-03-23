1569b458f2c3517affa75a2e35cee1ea9736bba7
Edward Wallitt <edwallitt@me.com>
2015-03-23 09:37:13 +0000

    add direct downloads - finally

--- app/controllers/hosted_files_controller.rb
+++ app/controllers/hosted_files_controller.rb
@@ -30,7 +30,9 @@ class HostedFilesController < ApplicationController
       #bucket = s3.buckets['podmedics-test-harness']
       bucket = s3.buckets['podmedics-resources']
       object = bucket.objects[file_path]
-      url = object.url_for(:read, :expires => 10*60).to_s
+      url = object.url_for(:read, 
+                          :expires => 10.minutes,
+                          :response_content_disposition =>  'attachment;').to_s
     end
 
     def check_subscription
