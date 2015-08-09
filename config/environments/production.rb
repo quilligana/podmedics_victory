1cc429b5a32f724d4bfb5880dd6998fbc6854eb5
Edward Wallitt <edwallitt@me.com>
2015-08-09 13:14:22 +0100

    fix for serving static images

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -32,7 +32,7 @@ PodmedicsVictory::Application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Generate digests for assets URLs, required to be true for CDN Sumo (fastly)
   config.assets.digest = true
